1b159cfad254e97b41df044747ccc9a87967fe31
Mateus Elias <meap@cin.ufpe.br>
2023-10-12 03:37:45 -0300

    feat(models/user.rb): reorder fields and modify phone format error msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, presence: true, uniqueness: true
-    validates :cpf, cpf: true, numericality: { only_integer: true }, presence: true, uniqueness: true
     validates :phone, presence: true, uniqueness: true
+    validates :cpf, cpf: true, numericality: { only_integer: true }, presence: true, uniqueness: true
     validate :phone_format
 
     private
 
     def phone_format
         unless TelephoneNumber.parse(phone, :br).valid?
-            errors.add(:phone, 'is not a valid phone number')
+            errors.add(:phone, 'inválido')
         end
     end
 end
